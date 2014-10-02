eb9388a48c372e58c1b5a23ecb9da8d4fc5986f3
Shigenori Taya <shigenori.taya@gmail.com>
2014-10-02 20:32:56 +0900

    add some fixes to normal_distribution

--- hypothesis_testing/normal_distribution.py
+++ hypothesis_testing/normal_distribution.py
@@ -1,25 +1,32 @@
 #!/bin/python
 # -*- coding: shift-jis -*-
 
-SAMPLE_BORDER = 100
-
 import sys
 import math
 
-class nDistribution:
+DISTRIBUTION_Z = 'z'
+DISTRIBUTION_T = 't'
+
+class zDistribution:
     def __init__(self,
                  ):
+        self._name = DISTRIBUTION_Z
         self._table = False # TODO
         return
+    def get_distribution_name(self,):
+        return self._name
     def find_p(self, obs):
         return
 
 class tDistribution:
     def __init__(self, v, # degree of freedom
                  ):
+        self._name = DISTRIBUTION_T
         self._v = v
         self._table = False # TODO
         return
+    def get_distribution_name(self,):
+        return self._name
     def find_p(self, obs):
         try:
             table = self._tables[self._v]
@@ -29,19 +36,29 @@ class tDistribution:
         return
 
 class NormalDistribution:
-    def __init__(self, data, parent_mean, parent_var = False):
-        self._n = len(data)
-        if parent_var != False: # •ê•ªU‚ªŠù’m
-            self._dist = nDistribution()
-            self._obs = self.compute_z(data, parent_mean, parent_var)
-        else: # •ê•ªU‚ª–¢’m
-            if self._n >= SAMPLE_BORDER:
-                self._dist = nDistribution()
-                self._obs = self.compute_z(data, parent_mean, False)
-            else:
-                self._dist = tDistribution(v = n-1)
-                self._obs = self.compute_t(data, parent_mean)
+    SAMPLE_BORDER = 100
+
+    def __init__(self, samples, parent_mean, parent_var = False):
+        n = len(samples)
+        self._dist = self.get_dist(parent_var, n)
+        self._ktr = self.compute_ktr(samples, parent_mean, parent_var)
         return
+    def get_distribution(self, parent_var, n):
+        if parent_var != False: # •ê•ªU‚ªŠù’m
+            return zDistribution()
+        # •ê•ªU‚ª–¢’m
+        if n >= self.SAMPLE_BORDER:
+            # ³‹K•ª•z‚Å‹ß—‰Â”\
+            return zDistribution()
+        return tDistribution()
+    def compute_ktr(self,): # ktr: Kentei Toukei Ryou
+        if self._dist.get_distribution_name() = DISTRIBUTION_Z:
+            return self.compute_z(samples, parent_mean, parent_var)
+        else if self._dist.get_distribution_name() = .DISTRIBUTION_T:
+            return self.compute_t(samples, parent_mean, False)
+        else:
+            sys.stderr.write("unknown distribution!\n")
+            sys.exit()
     def is_significant(self, significant_level, both_sided = True):
         p = self._dist.find_p(self._obs)
         if both_sided:
