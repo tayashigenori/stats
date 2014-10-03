82f95d1bd5aa99ee483bbf2dfff12af1f0d3855f
Shigenori Taya <shigenori.taya@gmail.com>
2014-10-03 19:34:25 +0900

    add parent class in normal distribution

--- hypothesis_testing/normal_distribution.py
+++ hypothesis_testing/normal_distribution.py
@@ -7,35 +7,50 @@ import math
 DISTRIBUTION_Z = 'z'
 DISTRIBUTION_T = 't'
 
-class zDistribution:
+class distribution:
+    def get_distribution_name(self,):
+        return self._name
+    def find_p(self, obs):
+        try:
+            p = self._table[obs]
+        except IndexError:
+            sys.stderr.write("v (degree of freedom) not found.\n")
+            sys.exit()
+        return
+    def compute_ktr(self, samples): # ktr Kentei Toukei Ryou
+        # override me
+        return
+    def is_significant(self, ktr,
+                       significant_level, both_sided = True):
+        p = self.find_p(ktr)
+        if both_sided:
+            return p > (significant_level / 2.0)
+        return p > significant_level
+
+class zDistribution(distribution):
     def __init__(self,
                  ):
         self._name = DISTRIBUTION_Z
         self._table = False # TODO
         return
-    def get_distribution_name(self,):
-        return self._name
-    def find_p(self, obs):
+    def compute_ktr(self, samples,
+                    parent_mean, parent_var): # ktr Kentei Toukei Ryou
+        # TODO
         return
 
-class tDistribution:
+class tDistribution(distribution):
     def __init__(self, v, # degree of freedom
                  ):
         self._name = DISTRIBUTION_T
         self._v = v
         self._table = False # TODO
         return
-    def get_distribution_name(self,):
-        return self._name
-    def find_p(self, obs):
-        try:
-            table = self._tables[self._v]
-        except IndexError:
-            sys.stderr.write("v (degree of freedom) not found.\n")
-            sys.exit()
+    def compute_ktr(self, samples
+                    parent_mean): # ktr Kentei Toukei Ryou
+        # TODO
         return
 
-class NormalDistribution:
+class DistributionDispatcher:
     SAMPLE_BORDER = 100
 
     def __init__(self, samples, parent_mean, parent_var = False):
@@ -43,7 +58,7 @@ class NormalDistribution:
         self._dist = self.get_dist(parent_var, n)
         self._ktr = self.compute_ktr(samples, parent_mean, parent_var)
         return
-    def get_distribution(self, parent_var, n):
+    def dispatch_distribution(self, parent_var, n):
         if parent_var != False: # •ê•ªŽU‚ªŠù’m
             return zDistribution()
         # •ê•ªŽU‚ª–¢’m
@@ -51,19 +66,6 @@ class NormalDistribution:
             # ³‹K•ª•z‚Å‹ßŽ—‰Â”\
             return zDistribution()
         return tDistribution()
-    def compute_ktr(self,): # ktr: Kentei Toukei Ryou
-        if self._dist.get_distribution_name() = DISTRIBUTION_Z:
-            return self.compute_z(samples, parent_mean, parent_var)
-        else if self._dist.get_distribution_name() = .DISTRIBUTION_T:
-            return self.compute_t(samples, parent_mean, False)
-        else:
-            sys.stderr.write("unknown distribution!\n")
-            sys.exit()
-    def is_significant(self, significant_level, both_sided = True):
-        p = self._dist.find_p(self._obs)
-        if both_sided:
-            return p > (significant_level / 2.0)
-        return p > significant_level
 
 
 def main():
